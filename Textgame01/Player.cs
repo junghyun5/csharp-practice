0ac8b7c519f326cdeeb1de569dd64178be0d371f
daummm <50vjtpsxm@naver.com>
2025-02-12 20:09:46 -0600

    textgame01

---
+++ Textgame01/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG02
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+    internal class Player : Creature
+    {
+        protected PlayerType type;
+        
+
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+
+       
+        public PlayerType GetPlayerType() { return type; }
+        
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
+
---
+++ Textgame01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TextRPG02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Modify file name by pressing f2 button
+           Game game = new Game();
+
+            while(true)
+            {
+                game.Process();
+            }
+
+        }
+    }
+}
